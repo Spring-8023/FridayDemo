df43fbf45342c28aecaa1cebfa3f4405f09ba421
Spring-J <1517437552@qq.com>
2022-03-30 19:03:28 +0800

    注释掉allure启动服务

--- Friday/App/InterfaceAutomation/common/AllureHandler.py
+++ Friday/App/InterfaceAutomation/common/AllureHandler.py
@@ -5,7 +5,6 @@ Author：Spring
 Time：  2022/3/28 7:06 PM
 
 """
-
 from subprocess import call
 from config import settings
 from common.LoggerHandler import logger
@@ -15,8 +14,9 @@ class AllureHandler(object):
     def execute_command(self):
         try:
             call(settings.ALLURE_COMMAND, shell=True)
-            call(settings.ALLURE_START_COMMAND, shell=True)
+            # call(settings.ALLURE_START_COMMAND, shell=True)
             logger().info('执行allure命令成功')
+
 
         except Exception as e:
             logger().error("执行allure命令失败，详情参考：{}".format(e))
